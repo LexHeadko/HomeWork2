135d1a7998ea8e6b5da03049fcdbe922d2a53046
LexHeadko <n-odes11rus@yandex.ru>
2023-01-14 13:26:18 +0200

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -40,5 +40,11 @@ public class Main {
 
     public static void task4() {
         System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println("friend=" + friend);
+        friend = friend +2;
+        System.out.println("friend=" + friend);
+        friend = friend / 7;
+        System.out.println("friend=" + friend);
     }
 }
