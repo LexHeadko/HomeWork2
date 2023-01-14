607bcc124310ad24820e4a04c1331cd8a9bcf155
LexHeadko <n-odes11rus@yandex.ru>
2023-01-14 13:11:47 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,13 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper +4;
+        System.out.println("dog=" + dog + "," + "cat=" + cat + "," + "paper=" + paper);
          // Пишем код для задачи 2
     }
 }
